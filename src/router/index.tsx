552284383f9694de02fbe040eb1882f63c7281bc
Loui <1258659851@qq.com>
2024-04-22 16:26:19 +0800

    fix: 🧩 测试

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -5,6 +5,7 @@ import { request } from '../utils/request'
 import getTargetCookie from '../utils/getTargetCookie'
 import ErrorPage from '../pages/ErrorPage/ErrorPage'
 import Home from '../pages/Home'
+import RankList from '../pages/RankList'
 // meta 里的属性类型
 interface metaType {
   auth?: boolean // auth：是否需要路由守卫进行身份验证
@@ -43,10 +44,11 @@ export const routes: RouteObject[] = [
       },
       {
         path: '/rankList',
-        lazy: async () => {
-          const RankList = await import('../pages/RankList')
-          return { Component: RankList.default }
-        },
+        element: <RankList />,
+        // lazy: async () => {
+        //   const RankList = await import('../pages/RankList')
+        //   return { Component: RankList.default }
+        // },
       },
       {
         path: 'singerList',

406bfc29fd6a57f0253e04c7839a4ebd5c65f868
Loui <1258659851@qq.com>
2024-04-23 12:51:15 +0800

    build: 📦️ 修复 external

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-router', 'react-router-dom', 'react-redux'],
-      output: {},
-    },
-  },
   server: {
     proxy: {
       '/api': {

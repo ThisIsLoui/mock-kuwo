aeee6be2a1e25faa108c559262da76d0c25c418c
Loui <1258659851@qq.com>
2024-04-23 12:27:55 +0800

    build: 📦️ 修改 external

--- vite.config.ts
+++ vite.config.ts
@@ -6,12 +6,8 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['react', 'react-router', 'react-router-dom', 'react-redux'],
-      output: {
-        globals: {
-          react: 'React',
-        },
-      },
+      external: ['react-router', 'react-router-dom', 'react-redux'],
+      output: {},
     },
   },
   server: {

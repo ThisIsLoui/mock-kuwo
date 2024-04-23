102a75a65f3c0409fd5250b34a5d250b2bb7cd81
Loui <1258659851@qq.com>
2024-04-23 12:11:15 +0800

    build: 📦️ 配置 vite 不要打包 react、reactrouter、redux

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['react', 'react-router', 'react-router-dom', 'react-redux'],
+      output: {
+        globals: {
+          react: 'React',
+        },
+      },
+    },
+  },
   server: {
     proxy: {
       '/api': {
